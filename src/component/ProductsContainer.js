4fd597ce964f325e179fdd0aa5f1df974937344a
Nalin Padmasiri <nalinsasika9@gmail.com>
2025-08-31 15:52:38 +0800

    Products container implemented using use selector and use dispatch

--- src/component/ProductsContainer.js
+++ src/component/ProductsContainer.js
@@ -1,29 +1,39 @@
-import { connect } from "react-redux";
-import { fetchProducts } from "./redux/product/ProductActions";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "./redux/product/ProductActions";
 
-const ProductsContainer = ({ fetchProducts, productsData }) => {
+// later implemented using hooks, initailly using connect HOC
+const ProductsContainer = () => {
+  const productsData = useSelector((state) => state.product);
+  const dispatch = useDispatch();
   useEffect(() => {
-    fetchProducts();
+    dispatch(fetchProducts());
   }, []);
   console.log(productsData);
-  return <div>
-    {productsData.loading &&  <div>Loading....</div>}
-    {productsData.error && <p>{productsData.error}</p>}
-    {productsData.products && productsData.products.map(product => <p key={product.id}>{product.title}</p>)}    
-  </div>;
+  return (
+    <div>
+      {productsData.loading && <div>Loading....</div>}
+      {productsData.error && <p>{productsData.error}</p>}
+      {productsData.products &&
+        productsData.products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    productsData: state.product
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     productsData: state.product
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchProducts: () => dispatch(fetchProducts()),
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     fetchProducts: () => dispatch(fetchProducts()),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductsContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(ProductsContainer);
+
+export default ProductsContainer;
